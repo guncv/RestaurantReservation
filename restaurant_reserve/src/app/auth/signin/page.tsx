c1ce6a37d91f9e69b9507389602470b0b081f5fe
guncv <chanagun.vir@gmail.com>
2024-01-22 16:40:35 +0700

    22/01/2024 04:40PM
    Customize SignIn Form For Authorization

--- restaurant_reserve/src/app/auth/signin/page.tsx
+++ restaurant_reserve/src/app/auth/signin/page.tsx
@@ -1,7 +1,63 @@
+'use client'
+import { useState } from "react"
+import styles from "@/styles/Font.module.css"
+import Image from "next/image"
+import Link from "next/link"
+
 export default function Signin(){
+    const [showPassword,setShowPassword] = useState(false);
+    const [password,setPassword] = useState("");
+    const [email,setEmail] = useState("");
+
     return (
-        <div className="w-screen h-screen justify-center items-center">
-            qdqdqdqdw
+        <div className={`${styles.fontQuestrial} w-screen h-screen flex justify-center items-center bg-slate-300`}>
+            <div className="w-[400px] h-[460px] border-[1px] flex justify-center rounded-2xl opacity-50
+            bg-slate-200 mt-[130px] flex-wrap">
+                <div className="h-[35%] text-center w-full text-[25px]"> 
+                    <div className="w-full justify-center flex items-end h-[70%]">
+                        <Image className="w-[150px] h-full"
+                        src="/img/profile.png"
+                        alt="Error to download profile"
+                        width = {1000}
+                        height = {1000}/>
+                    </div>
+                    
+                    <div className="w-full text-center h-[30%]">
+                        MEMBER LOGIN
+                    </div>
+                </div>
+
+                <div className="h-[45%] w-full flex justify-center items-end flex-wrap">
+                    <form action="" className="w-full">
+                        <div className="text-center">
+                            <input className="w-[75%] h-[50px] indent-5" type="email" placeholder="Email ID"
+                            onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
+
+                        <div className="text-center">
+                            <input className="w-[75%] h-[50px] indent-5 mt-[20px]" type={showPassword? "text" : "password"} placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}/>
+                            <span className="cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
+                                {
+                                showPassword? <h1>Hide Password</h1> :
+                                <h1>Show Password</h1>
+                                }
+                            </span>
+                        </div>
+                        
+                        <div className="text-center w-full mt-[23px]">
+                            <button className="text-center h-[45px] text-[15px] bg-slate-700 text-white w-[50%] rounded-xl
+                            hover:bg-slate-600 duration-150 ">
+                                LOGIN
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+                <div className="h-[20%] flex w-full justify-center items-center">
+                    <h1>Don't Have Account Yet? <Link className="underline text-blue-900 hover:text-blue-700" href={"/"}>Register</Link> Here</h1>
+                </div>
+            </div>
         </div>
     )
 }
--- restaurant_reserve/src/app/auth/signout/page.tsx
+++ restaurant_reserve/src/app/auth/signout/page.tsx
@@ -1,24 +1,37 @@
 'use client'
 import { signOut, useSession } from "next-auth/react"
 import styles from "@/styles/Font.module.css"
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 
 export default function SignOut(){
-    const {data:session} = useSession();  
+    const router = useRouter();
+    const {data:session} = useSession();
+
+    const handlerSignout = async() => {
+        if (session) {
+            await signOut({redirect:false})
+        } 
+        setTimeout(() => {
+            window.location.reload();
+        }, 2000);
+        router.push("/");
+    }
 
     return (
         <div className={`${styles.fontQuestrial} w-screen h-screen justify-center flex items-center`}>
-            <div className="bg-white w-[500px] h-[300px] border-black border-[1px] flex 
-            flex-wrap justify-center items-center">
-                <div className="text-[50px] text-center w-full">
+            <div className="bg-white w-[500px] h-[250px] border-black border-[1px] flex 
+            flex-wrap justify-center">
+                <div className="text-[50px] h-[50%] w-full flex items-center justify-center">
                     SIGN OUT 
                 </div>
-                <div className="text-[20px] w-full text-center text-slate-400">
-                    Are you sure to sign out from {session?.user.name}
+                <div className="text-[20px] h-[25%] w-full text-slate-400 flex justify-center items-center">
+                    Are you sure to Sign out ?
                 </div>
-                <div className="flex justify-center w-full">
+                <div className="flex justify-center w-full h-[25%]">
                     <button className="bg-slate-600 text-white text-[20px] w-[80%] h-[50px] rounded-full border-[1px] border-slate-100
-                    hover:bg-red-100 hover:text-red-500 hover:border-red-600 hover:scale-110 hover:font-bold duration-150">
+                    hover:bg-red-100 hover:text-red-500 hover:border-red-600 hover:scale-110 hover:font-bold duration-150"
+                    onClick={handlerSignout}>
                         SIGN OUT
                     </button>
                 </div>
